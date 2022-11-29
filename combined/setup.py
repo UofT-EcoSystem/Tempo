554330df402110d179576a78c3d86855cd3a1c9f
Zhanda Zhu <zhandazhu@gmail.com>
2022-11-28 21:50:30 -0500

    remove original folder structure

--- combined/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-setup(name='combined',
-      ext_modules=[cpp_extension.CppExtension(name='combined_cpp', 
-                                              sources=['combined.cpp'],
-                                              extra_compile_args=['-Ofast'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
